ec1b8b6755b613ae0496f5f33994e24b443a6de5
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2020-11-30 19:49:37 -0300

    Adicionar Membros

---
+++ lib/telas/AddGroupMember.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import '../model/GroupModel.dart';
+import '../model/Usuario.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AddGroupMember extends StatefulWidget {
+  Group grupo;
+  AddGroupMember(this.grupo);
+  @override
+  _AddGroupMemberState createState() => _AddGroupMemberState();
+}
+
+class _AddGroupMemberState extends State<AddGroupMember> {
+  String _idUsuarioLogado;
+  String _emailUsuarioLogado;
+
+  Future<List<Usuario>> _recuperarContatos() async {
+    Firestore db = Firestore.instance;
+
+    QuerySnapshot querySnapshot =
+        await db.collection("usuarios").getDocuments();
+
+    List<Usuario> listaUsuarios = List();
+    for (DocumentSnapshot item in querySnapshot.documents) {
+      var dados = item.data;
+      if (dados["email"] == _emailUsuarioLogado) continue;
+
+      Usuario usuario = Usuario();
+      usuario.idUsuario = item.documentID;
+      usuario.email = dados["email"];
+      usuario.nome = dados["nome"];
+      usuario.urlImagem = dados["urlImagem"];
+
+      listaUsuarios.add(usuario);
+    }
+
+    return listaUsuarios;
+  }
+
+  _insereUsuarioGrupo(Usuario usuario) async {
+    widget.grupo.Membros.add(usuario);
+    widget.grupo.save();
+  }
+
+  _recuperarDadosUsuario() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirebaseUser usuarioLogado = await auth.currentUser();
+    _idUsuarioLogado = usuarioLogado.uid;
+    _emailUsuarioLogado = usuarioLogado.email;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarDadosUsuario();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Usuario>>(
+      future: _recuperarContatos(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text("Carregando contatos"),
+                  CircularProgressIndicator()
+                ],
+              ),
+            );
+            break;
+          case ConnectionState.active:
+          case ConnectionState.done:
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (_, indice) {
+                  List<Usuario> listaItens = snapshot.data;
+                  Usuario usuario = listaItens[indice];
+
+                  return ListTile(
+                    onTap: () {
+                      _insereUsuarioGrupo(usuario);
+                    },
+                    contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                    leading: CircleAvatar(
+                        maxRadius: 30,
+                        backgroundColor: Colors.grey,
+                        backgroundImage: usuario.urlImagem != null
+                            ? NetworkImage(usuario.urlImagem)
+                            : null),
+                    title: Text(
+                      usuario.nome == null ? "" : usuario.nome,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  );
+                });
+            break;
+        }
+      },
+    );
+  }
+}
